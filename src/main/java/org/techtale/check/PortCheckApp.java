e2817e72e6013ef65ae2461524834007fe3a0868
Harimohan Rajamohanan <harimohan.rajamohanan@wipro.com>
2023-06-26 18:21:00 +0530

    Added the base

---
+++ src/main/java/org/techtale/check/PortCheckApp.java
@@ -0,0 +1,13 @@
+package org.techtale.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortCheckApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortCheckApp.class, args);
+	}
+
+}
